64851c08b9fad9631e7fb21bf77cca25c2f25c39
Anna <mingyum119@naver.com>
2025-04-10 13:00:10 +0200

    Step3 - Lotto (Second Prize) (#1156)
    * feat: enter bonus number for second prize
    
    * feat: calculate winning statistics considering bonus number
    
    * test: fix error and write test code
    
    ---------
    
    Co-authored-by: MingyumKim <mingyum.kim@deliveryhero.com>

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -1,12 +1,13 @@
 package lotto.view
 
+import lotto.domain.MatchPrize
 import lotto.domain.Ticket
 import lotto.domain.Tickets
 import lotto.domain.WinningStatistics
 
 class ResultView {
     fun printTickets(tickets: Tickets) {
-        println("You have purchased ${tickets.size()}")
+        println("You have purchased ${tickets.size}")
         for (ticket in tickets.tickets) {
             printTicket(ticket)
         }
@@ -16,11 +17,12 @@ class ResultView {
         println(ticket.lottoNumber.map { it.number }
             .joinToString(", ", "[", "]"))
     }
-
     fun printWinningStatistics(winningStatistics: WinningStatistics, rate: Double) {
         println("\nWinning Statistics\n------------------")
-        for(entry in WinningStatistics.matchesPrice) {
-            println("${entry.key} Matches (${entry.value} KRW) - ${winningStatistics.get(entry.key)} ticket")
+        for(prize in MatchPrize.entries) {
+            println("${prize.matchCount} Matches " +
+                    (if (prize.usingBonus) "+ Bonus Ball " else "") +
+                    "(${prize.price} KRW) - ${winningStatistics.get(prize.matchCount, prize.usingBonus)} tickets")
         }
         println("Total return rate is $rate (A rate below 1 means a loss)")
     }
