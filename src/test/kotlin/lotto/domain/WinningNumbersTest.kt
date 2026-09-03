962c8f6dc0b2f7dac9b072da70cc3aef444518a3
Jaeyeon Cho <cjyeon1022@gmail.com>
2026-09-03 14:07:34 +0900

    test: 보너스 번호 조건 정의

--- src/test/kotlin/lotto/domain/WinningNumbersTest.kt
+++ src/test/kotlin/lotto/domain/WinningNumbersTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
@@ -13,6 +14,26 @@ class WinningNumbersTest {
     @Test
     fun `당첨 번호는 1부터 45 사이여야 한다`() {
         assertThatThrownBy { WinningNumbers.from(listOf(1, 2, 3, 4, 5, 46)) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `보너스 번호를 함께 관리한다`() {
+        val winningNumbers = WinningNumbers.from(listOf(1, 2, 3, 4, 5, 6), bonusNumber = 7)
+
+        assertThat(winningNumbers.matchesBonus(7)).isTrue()
+        assertThat(winningNumbers.matchesBonus(8)).isFalse()
+    }
+
+    @Test
+    fun `보너스 번호가 범위를 벗어나면 생성할 수 없다`() {
+        assertThatThrownBy { WinningNumbers.from(listOf(1, 2, 3, 4, 5, 6), bonusNumber = 46) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `보너스 번호가 당첨 번호와 중복되면 생성할 수 없다`() {
+        assertThatThrownBy { WinningNumbers.from(listOf(1, 2, 3, 4, 5, 6), bonusNumber = 6) }
             .isInstanceOf(IllegalArgumentException::class.java)
     }
 }
